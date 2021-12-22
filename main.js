162c83536c957c75a4eb5f5d3b64b9b70deb2fcb
Sota Hatakeyama <choo.bla.rin@gmail.com>
2021-12-22 16:21:46 +0900

    Demo of vega-lite-api

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import embed from "vega-embed";
-import { spec } from "./spec";
+import * as vl from "vega-lite-api";
+import movieData from "./data/movies.json";
 import "./style.css";
+
+const spec = vl
+  .markBar()
+  .data(movieData)
+  .encode(
+    vl.x().fieldQ("IMDB Rating").bin(true),
+    vl.y().count()
+  )
+  .toObject();
 
 embed("#app", spec, { renderer: "svg" });
