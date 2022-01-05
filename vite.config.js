e321768318878a83fc70c409a69e2c466a558b7f
Sota Hatakeyama <choo.bla.rin@gmail.com>
2022-01-05 13:11:18 +0900

    Aboutページを追加するデモ

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-export default {
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
+
+module.exports = defineConfig({
   base: "/learn-data-visualization/",
   build: {
     outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about/index.html"),
+      },
+    },
   },
-};
+});
