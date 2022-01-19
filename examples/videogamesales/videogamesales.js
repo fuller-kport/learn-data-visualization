81c54d6ad3078d360a7e1dd123c7dcc9f6f4c894
Sota Hatakeyama <choo.bla.rin@gmail.com>
2022-01-19 17:54:36 +0900

    ビデオゲームの売り上げのグラフを表示

--- examples/videogamesales/videogamesales.js
+++ examples/videogamesales/videogamesales.js
@@ -1,9 +1,30 @@
+import embed from "vega-embed";
+import * as vl from "vega-lite-api";
+
 fetch("/vgsales.csv")
   .then((response) => response.text())
   .then(parseVideoGameSalesCSV)
-  .then((results) => {
-    console.log(results);
-  });
+  .then(renderChart);
+
+function renderChart(data) {
+  const spec = vl
+    .markBar()
+    .width(640)
+    .height(400)
+    .data(data)
+    .transform(vl.filter(`datum["Global_Sales"] <= 4`))
+    .encode(
+      vl
+        .x()
+        .fieldQ("Global_Sales")
+        .bin({ step: 0.1 })
+        .title("Global Sales (in millions)"),
+      vl.y().count()
+    )
+    .toObject();
+
+  embed("#chart", spec);
+}
 
 function parseVideoGameSalesCSV(text) {
   const [, ...rest] = text.split("\n");
@@ -16,31 +37,31 @@ function parseVideoGameSalesCSV(text) {
     }
 
     const [
-      rank,
-      name,
-      platform,
-      year,
-      genre,
-      publisher,
-      naSales,
-      euSales,
-      jpSales,
-      otherSales,
-      globalSales,
+      Rank,
+      Name,
+      Platform,
+      Year,
+      Genre,
+      Publisher,
+      NA_Sales,
+      EU_Sales,
+      JP_Sales,
+      Other_Sales,
+      Global_Sales,
     ] = line.split(",");
 
     const item = {
-      rank: +rank,
-      name,
-      platform,
-      year: +year,
-      genre,
-      publisher,
-      naSales: +naSales,
-      euSales: +euSales,
-      jpSales: +jpSales,
-      otherSales: +otherSales,
-      globalSales: +globalSales,
+      Rank: +Rank,
+      Name,
+      Platform,
+      Year: +Year,
+      Genre,
+      Publisher,
+      NA_Sales: +NA_Sales,
+      EU_Sales: +EU_Sales,
+      JP_Sales: +JP_Sales,
+      Other_Sales: +Other_Sales,
+      Global_Sales: +Global_Sales,
     };
 
     results.push(item);
